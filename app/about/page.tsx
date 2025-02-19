eef8fca86467211fb439e50e2cca6a4fcd7ea817
MykhailoKondrat <mykhailo.kondrat@gmail.com>
2025-02-19 14:39:55 +0100

    add tilt

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,11 @@
 import { Avatar } from '@/components';
 import { Description, Headline } from './sections';
+import { Links } from './sections/Links';
 export default function About() {
 	return (
-		<div className="group [max-h-[100vh] overflow-y-hidden h-[100vh] pt-40">
-			<div className="max-w-[75%] w-[75%] mx-auto  h-[100%]">
-				<div className="flex flex-col gap-8 items-center sm:items-start">
+		<div className="group [max-h-[100vh] overflow-y-hidden h-[100vh] w-full pt-40">
+			<div className="max-w-[85%] w-[85%] mx-auto h-[100%]">
+				<div className="flex flex-col gap-8 items-center max-lg:ml-0 ml-[12vw] sm:items-start">
 					<div className="grid grid-cols-3 gap-4 w-full grid-rows-[auto_1fr]">
 						<div className="col-span-1 max-lg:col-start-2 max-lg:place-self-center ">
 							<div className="aspect-square h-full w-full">
@@ -13,6 +14,10 @@ export default function About() {
 						</div>
 						<Headline />
 						<Description />
+
+						<div className="col-span-2 col-start-2 flex flex-col items-left z-20 max-lg:col-span-3">
+							<Links />
+						</div>
 					</div>
 				</div>
 			</div>
--- app/about/sections/Description.tsx
+++ app/about/sections/Description.tsx
@@ -1,7 +1,5 @@
-import { Links } from './Links';
-
 export const Description: React.FC = () => (
-	<div className="col-span-2 col-start-2 flex flex-col items-left z-20 max-lg:col-span-3">
+	<div className="pointer-events-none col-span-2 col-start-2 flex flex-col items-left z-20 max-lg:col-span-3">
 		<p className="opacity-0 animation-delay-800 animate-fadeIn text-2xl font-merry text-foreground text-left my-5 max-lg:text-center max-lg:text-base">
 			Passionate software engineer and former UI/UX designer. <br />
 			Currently working at{' '}
@@ -10,6 +8,5 @@ export const Description: React.FC = () => (
 			</a>
 			.
 		</p>
-		<Links />
 	</div>
 );
